3943de5e3fad3e9d43f52a127f876e4c6edd779a
pKononenko <44846303+pKononenko@users.noreply.github.com>
2020-05-02 21:08:25 +0300

    Update query.sql

--- query.sql
+++ query.sql
@@ -26,3 +26,35 @@ SElECT
 FROM GAMESALES
 GROUP BY year_of_release
 ORDER BY year_of_release;
+
+
+
+-- VIEW QUERIES
+-- FIRST QUERY
+
+SELECT 
+    game_name, 
+    SUM(REGION_SALES) AS TOTAL_SALES
+FROM GAMEDATA
+GROUP BY game_name
+ORDER BY TOTAL_SALES;
+
+
+-- SECOND QUERY
+
+SELECT
+    genre,
+    round( count(DISTINCT game_name) * 100 / ( SELECT count(*) FROM (SELECT DISTINCT GAME_NAME FROM GAMEDATA) ) , 2) AS PERCENTAGE
+FROM GAMEDATA
+GROUP BY genre
+ORDER BY PERCENTAGE DESC;
+
+
+-- THIRD QUERY
+
+SElECT 
+    year_of_release,
+    count(game_name)/4 AS GAMES_COUNT
+FROM GAMEDATA
+GROUP BY year_of_release
+ORDER BY year_of_release;
